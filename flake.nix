ec2b7db53f0336109785cdf11c84299484f83903
hugosenari <hugosenari@gmail.com>
2022-03-12 02:27:45 -0300

    chore: re index rabbit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''The Hundred Rabbits theme ecosystem brought to Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."rabbit-main".url = "path:./main";
+  inputs."rabbit-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."rabbit-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."rabbit-v0_1_0".url = "path:./v0_1_0";
+  inputs."rabbit-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."rabbit-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''The Hundred Rabbits theme ecosystem brought to Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-rabbit-main.flake = false;
+  inputs.src-rabbit-main.owner = "tonogram";
+  inputs.src-rabbit-main.ref   = "refs/heads/main";
+  inputs.src-rabbit-main.repo  = "rabbit";
+  inputs.src-rabbit-main.type  = "github";
+  
+  inputs."chroma".dir   = "nimpkgs/c/chroma";
+  inputs."chroma".owner = "riinr";
+  inputs."chroma".ref   = "flake-pinning";
+  inputs."chroma".repo  = "flake-nimble";
+  inputs."chroma".type  = "github";
+  inputs."chroma".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chroma".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-rabbit-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-rabbit-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
